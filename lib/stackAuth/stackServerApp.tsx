ae7ec4ef21e958d39d3a0f2b929ba1b113d3225d
Keming He <keminghe.career@gmail.com>
2024-12-17 16:34:26 -0500

    style(favicons): adjusted favicon sizes. Added formatting alignment to stack server app file

--- lib/stackAuth/stackServerApp.tsx
+++ lib/stackAuth/stackServerApp.tsx
@@ -12,12 +12,14 @@ const stackServerApp = new StackServerApp({
   tokenStore: 'nextjs-cookie',
 
   // Custom Stack Auth URLs.
+  // biome-ignore format: added alignment for clarity.
   urls: {
-    home: HOME_ROUTE,
-    signIn: AUTH_SIGN_IN_ROUTE,
-    afterSignIn: HOME_ROUTE,
-    afterSignUp: HOME_ROUTE,
-    afterSignOut: AUTH_SIGN_IN_ROUTE,
+    home             : HOME_ROUTE,
+    signIn           : AUTH_SIGN_IN_ROUTE,
+    signUp           : AUTH_SIGN_IN_ROUTE,
+    afterSignIn      : HOME_ROUTE,
+    afterSignUp      : HOME_ROUTE,
+    afterSignOut     : AUTH_SIGN_IN_ROUTE,
     emailVerification: AUTH_VERIFY_EMAIL_ROUTE,
     magicLinkCallback: AUTH_VERIFY_EMAIL_ROUTE,
   },
