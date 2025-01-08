1d9262e8e3cb8a00b8230a9a0919b42acc0bb7fc
Keming He <keminghe.career@gmail.com>
2025-01-07 23:23:53 -0500

    docs(legalnotice.tsx): added updated disclaimer, privacy, and terms md files to docs
    links will only start working once merged to main, hence temp disabling e2e tests that relies on
    links
    
    completes #19

---
+++ constants/externalLinkConstants.ts
@@ -0,0 +1,14 @@
+// Developer links.
+export const GITHUB_REPO_LINK: string =
+  'https://github.com/KemingHe/buckeye-gpt';
+export const DEVELOPER_GITHUB_LINK: string = 'https://github.com/KemingHe';
+export const DEVELOPER_LINKEDIN_LINK: string =
+  'https://www.linkedin.com/in/keminghe';
+
+// Legal document links.
+export const LEGAL_DISCLAIMER_LINK: string =
+  'https://github.com/KemingHe/buckeye-gpt/blob/main/docs/legal/legal-disclaimer.md';
+export const PRIVACY_POLICY_LINK: string =
+  'https://github.com/KemingHe/buckeye-gpt/blob/main/docs/legal/privacy-policy.md';
+export const TERMS_AND_CONDITIONS_LINK: string =
+  'https://github.com/KemingHe/buckeye-gpt/blob/main/docs/legal/terms-and-conditions.md';
