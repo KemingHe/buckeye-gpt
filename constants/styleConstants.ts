e2f23861d5e61b849820ad6357537b52d2d49275
Keming He <keminghe.career@gmail.com>
2025-01-09 21:43:57 -0500

    fix(layout.tsx): modularized socialify constants and use /png endpoint to fix og render logo issue

--- constants/styleConstants.ts
+++ constants/styleConstants.ts
@@ -1,2 +1,10 @@
 // Color constants.
 export const COLOR_PRIMARY: string = '#1d242a';
+
+// Preview image constants.
+export const SOCIALIFY_PREVIEW_IMAGE_URL: string =
+  'https://socialify.git.ci/KemingHe/buckeye-gpt/png?description=1&forks=1&issues=1&language=1&logo=https%3A%2F%2Fgithub.com%2FKemingHe%2Fbuckeye-gpt%2Fblob%2Fmain%2Fpublic%2Fimages%2Ftransparent-buckeye-gpt-icon-512x512px.png%3Fraw%3Dtrue&name=1&owner=1&pattern=Plus&pulls=1&stargazers=1&theme=Light';
+export const SOCIALIFY_PREVIEW_IMAGE_WIDTH: number = 1280;
+export const SOCIALIFY_PREVIEW_IMAGE_HEIGHT: number = 640;
+export const SOCIALIFY_PREVIEW_IMAGE_ALT: string =
+  'Buckeye GPT is a open-source multi-modal ChatGPT clone with RAG built free for students at the Ohio State University.';
