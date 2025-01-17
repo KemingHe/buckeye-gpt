2e27895d8278c8e2ac298846f3b2adf3c204165e
Keming He <keminghe.career@gmail.com>
2025-01-16 23:06:41 -0500

    feat(chatcontext.tsx): adopted ChatProvider for wrapper comps, retired prop drilling paradigm
    restricting ChatContext to only useChat-related values, which then depends on other hooks. adopted a
    loose coupling design and freed each sub-chat comp from relying on prop passing (drilling).
    
    completes #50, introduces #65, also completes #16

--- components/chatPage/ChatPageWrapper.tsx
+++ components/chatPage/ChatPageWrapper.tsx
@@ -4,20 +4,21 @@ import {
   useUser,
 } from '@stackframe/stack';
 import type { JSX } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
-import { ChatInputWrapper } from '@/components/chatInput/ChatInputWrapper';
-import { ChatMessagesWrapper } from '@/components/chatMessages/ChatMessagesWrapper';
+import ChatInputWrapper from '@/components/chatInput/ChatInputWrapper';
+import ChatMessagesWrapper from '@/components/chatMessages/ChatMessagesWrapper';
 import { ChatPageWireframe } from '@/components/chatPage/ChatPageWireframe';
+import { ChatProvider } from '@/contexts/ChatContext';
 
 export default function ChatPageWrapper(): JSX.Element {
   const clientUser: CurrentUser | CurrentInternalUser | null = useUser();
-  const chatId: string = uuidv4();
 
   return (
-    <ChatPageWireframe clientUser={clientUser}>
-      <ChatMessagesWrapper chatId={chatId} />
-      <ChatInputWrapper chatId={chatId} />
-    </ChatPageWireframe>
+    <ChatProvider>
+      <ChatPageWireframe clientUser={clientUser}>
+        <ChatMessagesWrapper />
+        <ChatInputWrapper />
+      </ChatPageWireframe>
+    </ChatProvider>
   );
 }
---
+++ contexts/ChatContext.tsx
@@ -0,0 +1,106 @@
+import {
+  type CurrentInternalUser,
+  type CurrentUser,
+  useUser,
+} from '@stackframe/stack';
+import { type Message, useChat } from 'ai/react';
+import {
+  type ChangeEvent,
+  type Context,
+  type FormEvent,
+  type JSX,
+  type ReactNode,
+  createContext,
+  useContext,
+  useMemo,
+} from 'react';
+
+import {
+  LANGCHAIN_OPENAI_LITE_API_ENDPOINT,
+  LANGCHAIN_OPENAI_REGULAR_API_ENDPOINT,
+} from '@/constants/apiEndpointConstants';
+
+// biome-ignore format: added alignment for clarity.
+export interface ChatContextValue {
+  // Chat state.
+  //  - Loading state.
+  isLoading         : boolean;
+
+  //  - Error state.
+  error             : Error | undefined;
+
+  //  - Message state.
+  messages          : Message[];
+
+  //  - (Form) Input state and handlers.
+  inputValue        : string;
+  setInputValue     : (value: string) => void;
+
+  //  - Form event handlers.
+  handleSubmit      : (event?: FormEvent<HTMLFormElement>) => void;
+  handleInputChange : (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  handleStopRequest : () => void;
+}
+
+const ChatContext: Context<ChatContextValue | undefined> = createContext<
+  ChatContextValue | undefined
+>(undefined);
+
+export function ChatProvider({
+  children,
+}: Readonly<{ children: ReactNode }>): JSX.Element {
+  const clientUser: CurrentUser | CurrentInternalUser | null = useUser();
+
+  const {
+    isLoading,
+    error,
+    messages,
+    input,
+    setInput,
+    handleSubmit,
+    handleInputChange,
+    stop,
+  } = useChat({
+    api: clientUser
+      ? LANGCHAIN_OPENAI_REGULAR_API_ENDPOINT
+      : LANGCHAIN_OPENAI_LITE_API_ENDPOINT,
+    onError: (e) => console.error(e),
+  });
+
+  const memoizedChatContextValue: ChatContextValue = useMemo(
+    () => ({
+      isLoading,
+      error,
+      messages,
+      inputValue: input,
+      setInputValue: setInput,
+      handleSubmit,
+      handleInputChange,
+      handleStopRequest: stop,
+    }),
+    [
+      isLoading,
+      error,
+      messages,
+      input,
+      setInput,
+      handleSubmit,
+      handleInputChange,
+      stop,
+    ],
+  );
+
+  return (
+    <ChatContext.Provider value={memoizedChatContextValue}>
+      {children}
+    </ChatContext.Provider>
+  );
+}
+
+export function useChatContext(): ChatContextValue {
+  const context: ChatContextValue | undefined = useContext(ChatContext);
+  if (context === undefined) {
+    throw new Error('useChatContext must be used within a ChatProvider');
+  }
+  return context;
+}
