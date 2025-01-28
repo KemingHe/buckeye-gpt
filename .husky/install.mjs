f6b55a0a1cbd1cefba9bde95b4c2efb93a5a8c0a
Keming He <keminghe.career@gmail.com>
2025-01-27 23:19:15 -0500

    fix(.husky/install.mjs): logged process.env to debug github action runs

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,6 +1,17 @@
 // Skip husky installation on CI.
 // https://typicode.github.io/husky/how-to.html#ci-server-and-docker
-if (process.env.NODE_ENV === 'production' || process.env.CI) {
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.CI ||
+  process.env.CI === 'true' ||
+  process.env.GITHUB_ACTIONS ||
+  process.env.GITHUB_ACTIONS === 'true'
+) {
+  console.log('CI environment detected:', {
+    NODE_ENV: process.env.NODE_ENV,
+    CI: process.env.CI,
+    GITHUB_ACTIONS: process.env.GITHUB_ACTIONS,
+  });
   console.log('Husky installation skipped.');
   process.exit(0);
 }
